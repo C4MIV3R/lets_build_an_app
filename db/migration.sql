5b87e15b462c7d2bb9a910bec5cb53bb2008b3dc
cam iverson <camiver@gmail.com>
2016-03-22 17:48:52 -0500

    migration.sql updated

---
+++ db/migration.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE build_an_app;
+\c build_an_app
+CREATE TABLE accounts (id SERIAL PRIMARY KEY, username VARCHAR(255), user_email VARCHAR(255), password_hash VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), friends_of_user INTEGER REFERENCES accounts (id));
+CREATE TABLE photos (id SERIAL PRIMARY KEY, photo_poster INTEGER REFERENCES accounts (id), photo_tags VARCHAR(255), photo_caption VARCHAR(140));
+CREATE TABLE comments (id SERIAL PRIMARY KEY, comment_poster INTEGER REFERENCES accounts (id), photo_posted_to INTEGER REFERENCES photos (id), comment_content VARCHAR(255));
